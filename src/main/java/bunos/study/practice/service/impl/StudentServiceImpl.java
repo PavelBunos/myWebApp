6e95afbfb7a4ee6d37ab3af4a11b6f0060f2525d
Pavel <pavel.games21@gmail.com>
2023-12-11 16:16:29 +0300

    feat: StudentService database implementation was added

---
+++ src/main/java/bunos/study/practice/service/impl/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package bunos.study.practice.service.impl;
+
+import bunos.study.practice.model.Student;
+import bunos.study.practice.repository.StudentRepository;
+import bunos.study.practice.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository repository;
+    @Override
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return repository.save(student);
+    }
+
+    @Override
+    public void deleteStudent(String email) {
+        repository.deleteByEmail(email);
+    }
+}
