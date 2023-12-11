6b3b9b8b7c7a57affa20cd1d66caf12ae4a7779a
Pavel <pavel.games21@gmail.com>
2023-12-11 15:57:06 +0300

    upd: Student class was converted into database entity

--- src/main/java/bunos/study/practice/model/Student.java
+++ src/main/java/bunos/study/practice/model/Student.java
@@ -1,14 +1,20 @@
 package bunos.study.practice.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
-@Builder
+@Entity
+@Table(name="students_table")
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private int age;
 }
